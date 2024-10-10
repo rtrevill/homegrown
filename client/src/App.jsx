ab0a40590580b903f61e1833a8d8726cb13e84e8
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-10-10 13:35:57 +1100

    added baseline code to get started

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: "/graphql"
+});
+
+
+const client = new ApolloClient({
+  // uri: '/graphql',
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Outlet />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
